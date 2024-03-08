cad19cfa7c2c0600a83841d08a6cb50c87197749
wael-saif23 <ws.mywork@gmail.com>
2024-03-08 06:38:36 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/store_app_tr2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.store_app_tr2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/products_modle.dart
@@ -0,0 +1,41 @@
+class ProductsModel {
+  final String id;
+  final String title;
+  final String price;
+  final String description;
+  final String category;
+  final String image;
+  final RatingModel rating;
+  ProductsModel({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.category,
+    required this.image,
+    required this.rating,
+  });
+
+  factory ProductsModel.fromjson(jsondata) {
+    return ProductsModel(
+        id: jsondata['id'],
+        title: jsondata['title'],
+        price: jsondata['price'],
+        description: jsondata['description'],
+        category: jsondata['category'],
+        image: jsondata['image'],
+        rating: RatingModel.formjson(
+          jsondata['rating'],
+        ));
+  }
+}
+
+class RatingModel {
+  final String rate;
+  final String count;
+
+  RatingModel({required this.rate, required this.count});
+  factory RatingModel.formjson(json) {
+    return RatingModel(rate: json['rate'], count: json['count']);
+  }
+}
