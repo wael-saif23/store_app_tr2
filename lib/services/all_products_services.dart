6fe1387acb9b0963f10e3ad3c1e5bfb458010675
wael-saif23 <ws.mywork@gmail.com>
2024-03-08 14:37:37 +0200

    create AllProductsServices

--- lib/services/all_products_services.dart
+++ lib/services/all_products_services.dart
@@ -1,3 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:store_app_tr2/models/products_modle.dart';
+
 class AllProductsServices {
-  
+  final Dio dio;
+
+  AllProductsServices({required this.dio});
+  Future<List<ProductsModel>> getAllProducts() async {
+    Response response = await dio.get("https://fakestoreapi.com/products");
+    List<dynamic> data = response.data;
+    List<ProductsModel> allProductsList = [];
+    for (var i = 0; i < data.length; i++) {
+      allProductsList.add(
+        ProductsModel.fromjson(data[i]),
+      );
+    }
+    return allProductsList;
+  }
 }
