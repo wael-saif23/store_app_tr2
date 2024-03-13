05d665cd18eacbc2489e2fce6e95b8338a20a11a
wael-saif23 <ws.mywork@gmail.com>
2024-03-14 01:01:27 +0200

    create home page

---
+++ lib/views/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  static String id = 'home page';
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
