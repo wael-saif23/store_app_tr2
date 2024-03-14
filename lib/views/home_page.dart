bfdd805e36b75cdf7a410bbfca4c6e63f8646468
wael-saif23 <ws.mywork@gmail.com>
2024-03-14 03:29:37 +0200

    edit body in home page

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -7,13 +7,44 @@ class HomePage extends StatelessWidget {
   static String id = 'home page';
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title:const Text('New Style'),
+        title: const Text('New Style'),
         elevation: 0,
         centerTitle: true,
         actions: [
-          IconButton(onPressed: (){}, icon:const Icon( FontAwesomeIcons.cartPlus))
+          IconButton(
+              onPressed: () {}, icon: const Icon(FontAwesomeIcons.cartPlus))
+        ],
+      ),
+      body: Stack(
+        children: [
+          Container(
+            child: Card(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "handpag lv",
+                    style: TextStyle(color: Colors.black54),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(r'$225'),
+                      IconButton(
+                        style: ButtonStyle(
+                            padding: MaterialStatePropertyAll(EdgeInsets.zero),
+                            tapTargetSize: MaterialTapTargetSize.shrinkWrap),
+                        onPressed: () {},
+                        icon: const Icon(Icons.favorite),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
